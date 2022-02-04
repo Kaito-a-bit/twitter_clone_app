9c99eeaae6422e83e83667e97cfc1fddd13cc812
Kaito-a-bit <arakai0128@gmail.com>
2022-02-04 13:55:35 +0900

    created base.html

--- user/urls.py
+++ user/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 app_name = "user"
 urlpatterns = [
-  path('signup/',views.SignUpView.as_view(), name='signup'),
-  path('',views.HomeView.as_view(), name='home'),
+  path('',views.HomeView.as_view(), name='base'),
+  path('signup/',views.SignUpView.as_view(), name='signup'),  
 ]
--- user/views.py
+++ user/views.py
@@ -5,10 +5,13 @@ from django.views.generic import TemplateView
 from django.urls import reverse_lazy
 from .forms import SignUpForm
 
+
+class HomeView(TemplateView):
+    template_name='user/base.html'
 class SignUpView(CreateView):
     form_class = SignUpForm
     template_name = 'user/sign_up.html'
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('user:home')
 
     def form_valid(self, form):
         user = form.save()
@@ -16,5 +19,4 @@ class SignUpView(CreateView):
         self.object = user
         return HttpResponseRedirect(self.get_success_url()) 
 
-class HomeView(TemplateView):
-    template_name='base.html'
+
