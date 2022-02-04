246c11c4cd3b46ec82f508d1f6a60ae07f2fc32e
Kaito-a-bit <arakai0128@gmail.com>
2022-02-04 09:54:54 +0900

    prepared UserCreationForm and views and url path associated with it.

--- user/urls.py
+++ user/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 app_name = "user"
 urlpatterns = [
-  path('signup/', views.SignupView.as_view(), name='signup'),
+  path('signup/',views.SignUpView.as_view(), name='signup'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,6 +1,16 @@
-from http.client import HTTPResponse
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from http.client import HTTPResponseRedirect
+from django.contrib.auth import login
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from .forms import SignUpForm
 
-class SignupView(TemplateView):
-    template_name = "user/sign_up.html"
+class SignUpView(CreateView):
+    form_class = SignUpForm
+    # template_name = テンプレート名
+    # success_url = reverse_lazy(ログイン成功時のurl)
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        self.object = user
+        return HTTPResponseRedirect(self.get_success_url()) 
