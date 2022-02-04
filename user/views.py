0dd11e2e8a140da984c34f45407ef67fc7cadcf2
Kaito-a-bit <arakai0128@gmail.com>
2022-02-04 10:13:37 +0900

    fixed syntac error.

--- user/views.py
+++ user/views.py
@@ -1,4 +1,4 @@
-from http.client import HTTPResponseRedirect
+from django.http import HttpResponseRedirect
 from django.contrib.auth import login
 from django.views.generic.edit import CreateView
 from django.views.generic import TemplateView
@@ -8,13 +8,13 @@ from .forms import SignUpForm
 class SignUpView(CreateView):
     form_class = SignUpForm
     template_name = 'sign_up.html'
-    # success_url = reverse_lazy(ログイン成功時のurl)
+    success_url = reverse_lazy('')
 
     def form_valid(self, form):
         user = form.save()
         login(self.request, user)
         self.object = user
-        return HTTPResponseRedirect(self.get_success_url()) 
+        return HttpResponseRedirect(self.get_success_url()) 
 
 class HomeView(TemplateView):
     template_name='base.html'
