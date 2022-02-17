fe1499251da1574dbe290d0175e6fa5cdda8957f
Kaito-a-bit <arakai0128@gmail.com>
2022-02-17 23:02:27 +0900

    fixed forms.py and views.py to correspond with  the changes made within models.py. This is the commit before SECOND migrations.

--- user/views.py
+++ user/views.py
@@ -1,25 +1,33 @@
 
+from dataclasses import fields
+from pyexpat import model
 from django.http import HttpResponseRedirect
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
 from django.views.generic.edit import CreateView
 from django.views.generic import TemplateView
 from django.urls import reverse_lazy
+from user.models import User
 from .forms import SignUpForm
 
 
 class BaseView(TemplateView):
     template_name = 'user/top.html'
 
+
 class HomeView(TemplateView):
     template_name = 'user/home.html'
     
+
 class SignUpView(CreateView):
     form_class = SignUpForm
     template_name = 'user/sign_up.html'
     success_url = reverse_lazy('user:home')
 
     def form_valid(self, form):
-        user = form.save()
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+
+        user = authenticate(username=username, password=password)
         login(self.request, user)
         self.object = user
         return HttpResponseRedirect(self.get_success_url()) 
