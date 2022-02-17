c7c32408c85d539f585f6c29d459771093e49219
Kaito-a-bit <arakai0128@gmail.com>
2022-02-17 13:17:31 +0900

    defined UsserManager.

--- user/models.py
+++ user/models.py
@@ -1,3 +1,4 @@
+from operator import truediv
 from pickle import FALSE
 from pyexpat import model
 from django.db import models
@@ -5,6 +6,35 @@ from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.utils.translation import gettext_lazy as _
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, username, email, password, **extra_fields):
+        if not email:
+          raise ValueError('ユーザ登録にはEmailアドレスの設定が必要になります')
+        email = self.normalize_email(email)
+        username = self.model.normalize_username(username)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self.db)
+        return user 
+
+    def create_user(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, username, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('is_staff=Trueである必要があります。')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('is_superuser=Trueである必要があります。')
+        return self._create_user(username, email, password, **extra_fields)
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     username_validator = UnicodeUsernameValidator()
@@ -17,3 +47,4 @@ class User(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = "email"
     EMAIL_FIELD = "email"
     REQUIRED_FIELDS = ['username']
+
