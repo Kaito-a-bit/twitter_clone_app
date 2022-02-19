9f7d59d9e43ab177ea4f1f23ffdbf963d028caf0
Kaito-a-bit <arakai0128@gmail.com>
2022-02-19 11:33:06 +0900

    added django-backup to this project.

--- twitter_clone_app/settings.py
+++ twitter_clone_app/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -32,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'user.apps.UserConfig',
+    'dbbackup',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -83,6 +85,10 @@ DATABASES = {
 
 AUTH_USER_MODEL = 'user.User'
 
+# 追加
+DBBACKUP_STORAGE = 'django.core.files.storage.FileSystemStorage'
+# BASE_DIR/backups/にバックアップファイルを保存する設定
+DBBACKUP_STORAGE_OPTIONS = {'location': os.path.join(BASE_DIR, 'backups')}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
