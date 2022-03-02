f1a18e4eb36cd9ac0a81b7782626fbf9ac20af91
Kaito-a-bit <arakai0128@gmail.com>
2022-03-02 10:35:37 +0900

    deleted password line.

--- user/forms.py
+++ user/forms.py
@@ -13,11 +13,9 @@ class SignUpForm(UserCreationForm):
             'username': 'ユーザネーム',
             'email': 'email',            
             'birthday': '誕生日',
-            'password': 'パスワード'
         }
         help_texts = {
             'username': '名前を入力してください（最大24文字）',
             'email': 'メールアドレスを入力してください',
             'birthday': '誕生日を入力してください（例: 1999-1-1）',
-            'password': 'パスワードを入力してください'
         }
