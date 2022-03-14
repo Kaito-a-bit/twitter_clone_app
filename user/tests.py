654162489d8936bd86410505b9a34ceee9110d80
Kaito-a-bit <arakai0128@gmail.com>
2022-03-14 15:52:28 +0900

    fixed some test codes

--- user/tests.py
+++ user/tests.py
@@ -1,5 +1,4 @@
-import datetime
-from typing_extensions import Self
+
 from django.test import TestCase, Client
 from django.urls import reverse
 from django.utils import timezone
@@ -7,41 +6,36 @@ from .models import User
 
 class TopViewTests(TestCase):
   def testStatus(self):
-    self.response = self.client.get(reverse('/'))
+    self.response = self.client.get('/')
     self.assertEqual(self.response.status_code, 200)
 
 
 class SignUpViewTests(TestCase):
   def testStatus(self):
-    self.response = self.client.get(reverse('/signup/'))
+    self.response = self.client.get('/signup/')
     self.assertEqual(self.response.status_code, 200)
 
 class RegisterErrorTests(TestCase):
-  def was_created_with_decent_date(self):
-    """
-    it returns False when an account is created with unreal date un the past.
-    ex: 1777-1-1
-    """
-    
-
-  def haa_birthday_in_the_future(self):
-    """
-    it returns False when an account is created in the future.
-    """
-    
-
   def has_too_short_password(self):
     """
     reject password shorter than 8 charactors.
     """
+    self.response = self.client.post('/signup/', {
+      'username': 'peter',
+      'email': 'peter0303@gmail.com',
+      'birthday': '199-1-1', 
+      'password': '0sv6d'})
+    self.assertEqual(self.response.status_code, 200)
   
-  def has_too_long_username(self):
-    """
-    reject username longer than 24 charactors.
-    """
-
-  def has_wrong_email(self):
-    """
-    reject email in the incorrect format.
-    """
+      
+  # def has_too_long_username(self):
+  #   """
+  #   reject username longer than 24 charactors.
+  #   """
+  
+  # def has_wrong_email(self):
+  #   """
+  #   reject email in the incorrect format.
+  #   """
+  
     
