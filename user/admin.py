e7c835748cf10a0d537f7a2e0d2d98bc7fe610de
Kaito-a-bit <arakai0128@gmail.com>
2022-03-17 18:37:58 +0900

    deletede redundant lines

--- user/admin.py
+++ user/admin.py
@@ -1,10 +1,4 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.forms import UserChangeForm, UserCreationForm
-from django.utils.translation import ugettext_lazy as _
 from .models import User
-
-class UserAdmin(admin.ModelAdmin):
-    pass
 
 admin.site.register(User)
