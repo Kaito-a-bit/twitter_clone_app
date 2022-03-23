b600e3e043cc09235f37ae7139ce0ffa3a1fa086
Kaito-a-bit <arakai0128@gmail.com>
2022-03-23 16:17:48 +0900

    created tweet model

--- tweet/models.py
+++ tweet/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.utils import timezone
+import uuid
 
-# Create your models here.
+class tweet(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    text = models.CharField(verbose_name='テキスト', max_length=140)
+    created_at = models.DateTimeField(verbose_name='投稿日時', default=timezone.now())
