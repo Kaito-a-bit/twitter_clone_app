fa708dcffc347d2b8b699e4b27dbaed1064d9e57
Kaito-a-bit <arakai0128@gmail.com>
2022-03-23 16:32:52 +0900

    added views.

--- tweet/views.py
+++ tweet/views.py
@@ -1,3 +1,10 @@
+from django import forms
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from tweet.forms import TweetForm
 
-# Create your views here.
+class TweetView(CreateView):
+    template_name = 'tweet/posting.html'
+    form_class = TweetForm
+    success_url = reverse_lazy('user/home.html')
