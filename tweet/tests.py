4f904fae420ca659db80e0d78c129191f8723dce
Kaito-a-bit <arakai0128@gmail.com>
2022-03-24 15:56:15 +0900

    added tests.py

--- tweet/tests.py
+++ tweet/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+# from django.test import TestCase
+# from django.urls import reverse
+# from .models import tweet
+# from user.models import User
 
-# Create your tests here.
+# class PostingViewTests(TestCase):
+#     def setUp(self):
+#         self.user = User.objects.create_user(username='testuser', email='test@gmail.com', password= 'ttt019283est')
+#         data = {
+#           'text': 'テステステス'
+#           'created_at': ''
+#         }
+
+
+#     def test_get_success(self):
+#         self.response = self.client.get(reverse('tweet'))
+#         self.assertEqual(self.response.status_code, 200)
+
+
