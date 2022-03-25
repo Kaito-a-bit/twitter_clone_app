f41b5082d66237f5ca4c499c5f9a50bb611a8264
Kaito-a-bit <arakai0128@gmail.com>
2022-03-25 16:14:13 +0900

    added codes to show username in the tweet.

--- tweet/forms.py
+++ tweet/forms.py
@@ -6,4 +6,4 @@ from .models import Tweet
 class TweetForm(forms.ModelForm):
     class Meta:
         model = Tweet
-        fields = ('text', 'created_at', 'author')
+        fields = ('text', 'created_at',)
--- tweet/views.py
+++ tweet/views.py
@@ -1,9 +1,20 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
+from tweet import forms
 from tweet.forms import TweetForm
+from .import forms
 
 class TweetView(CreateView):
     template_name = 'tweet/posting.html'
     form_class = TweetForm
     success_url = reverse_lazy('user:home')
+
+    def form_vaild(self, form, request):
+        form = forms.TweetForm(request.POST)
+        if form.is_valid():
+            tweet = form.save(commit=False)
+            tweet.author = request.user
+            tweet.save()
+            return HttpResponseRedirect(reverse_lazy('user:home'))
