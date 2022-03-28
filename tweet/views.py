306c48301ba852f0cd3147a29915585af9756454
Kaito-a-bit <arakai0128@gmail.com>
2022-03-28 18:08:23 +0900

    deleted redundant lines

--- tweet/views.py
+++ tweet/views.py
@@ -1,11 +1,6 @@
-
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
-from tweet import forms
 from tweet.forms import TweetForm
-from .import forms
 
 class TweetView(CreateView):
     template_name = 'tweet/posting.html'
@@ -14,6 +9,5 @@ class TweetView(CreateView):
     
     def form_valid(self, form):
         form.instance.author = self.request.user
-        print(form.instance.created_at)
         return super().form_valid(form)
         
