37e9aea642e3d72e9092c267fc2760ff756f5be3
Kaito-a-bit <arakai0128@gmail.com>
2022-03-28 18:28:57 +0900

    dropped table and did migration again.

--- tweet/models.py
+++ tweet/models.py
@@ -6,5 +6,5 @@ import uuid
 class Tweet(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     text = models.CharField(verbose_name='テキスト', max_length=140)
-    created_at = models.DateTimeField(verbose_name='投稿日時', default=timezone.now())
+    created_at = models.DateTimeField(verbose_name='投稿日時', default=timezone.now)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
