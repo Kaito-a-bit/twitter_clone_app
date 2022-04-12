a56b7131561ff743f5ea5ffcffaafbee6d7f149d
Kaito-a-bit <arakai0128@gmail.com>
2022-04-12 13:24:39 +0900

    created LikeModel. did migarations.

--- tweet/models.py
+++ tweet/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from user.models import User
 import uuid
 
 class Tweet(models.Model):
@@ -8,3 +9,8 @@ class Tweet(models.Model):
     text = models.CharField(verbose_name='テキスト', max_length=140)
     created_at = models.DateTimeField(verbose_name='投稿日時', default=timezone.now)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+class LikeModel(models.Model):
+    article = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(default=timezone.now)
